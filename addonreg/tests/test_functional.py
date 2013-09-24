3cab00400c0a296ea86ee91350e2e78b06bb7afa
Alexis Métaireau <alexis@notmyidea.org>
2013-09-24 17:45:14 +0200

    some tests coverage to test everything is working the way it should

---
+++ addonreg/tests/test_functional.py
@@ -0,0 +1,21 @@
+import os
+
+from unittest2 import TestCase
+import webtest
+
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+class FunctionalTest(TestCase):
+
+    def setUp(self):
+        self.app = webtest.TestApp("config:tests.ini", relative_to=HERE)
+        self.backend = self.app.app.registry.backend
+
+    def test_get_registered_addon(self):
+        addon_id = 'id@example.com'
+        hash_ = '0c27ec47b62e20a20a563aaada9dfa663d76a76f'
+        resp = self.app.get_json('/adddon/{addon_id}/{hash}'.format(
+            addon_id=addon_id, hash=hash_))
+        from pdb import set_trace; set_trace()
