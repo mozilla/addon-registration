0f29c8b1eeeac5f09fdc715bc6a3888ea0ad9bb5
Alexis Métaireau <alexis@notmyidea.org>
2013-09-24 18:52:04 +0200

    forgot to add the _python backend to git

---
+++ addonreg/backends/_python.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+
+class PythonBackend(object):
+    """A backend using python structured in memory to store data.
+
+    This doesn't really have any value, appart from the tests.
+    """
+
+    def __init__(self, config):
+        self.config = config
+        self._hashs = defaultdict(list)
+
+    def hash_exists(self, addon_id, hash_):
+        return hash_ in self._hashs[addon_id]
+
+    def register_hash(self, addon_id, hash_):
+        if hash_ not in self._hashs[addon_id]:
+            self._hashs[addon_id].append(hash_)
+
+    def empty(self):
+        """Empty the backend"""
+        self._hashs = defaultdict(list)
