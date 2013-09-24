fd47624865a76f1ffb7019193b8f97d811b9be1b
Alexis Métaireau <alexis@notmyidea.org>
2013-09-24 17:47:39 +0200

    pyramid doesn't like dashes

--- addonreg/views.py
+++ addonreg/views.py
@@ -1,15 +1,15 @@
 from cornice import Service
 
-addon = Service(name='addon', path='/addon/{addon-id}/{hash}')
+addon = Service(name='addon', path='/addon/{addonid}/{hash}')
 
 
 @addon.get()
 def get_addon(request):
     """Checks that an addon with the given addon-id and hash exists."""
-    addon_id = request.matchdict['addon-id']
+    addon_id = request.matchdict['addonid']
     hash_ = request.matchdict['hash']
 
-    response = {'addon-id': addon_id, 'sha256': hash_}
+    response = {'addonid': addon_id, 'sha256': hash_}
 
     registered = request.backend.hash_exists(addon_id, hash_)
     response['registered'] = registered
